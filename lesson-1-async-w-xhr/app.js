1e6876530cc55cda624ebd9cabe76bab91825a6c
pr-olga <frkhorkovaolga@gmail.com>
2018-07-03 21:48:29 +0200

    feat: request articles and images
    eventListener was extended by two requests

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -8,5 +8,47 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        //request articles
+        const articleRequest = new XMLHttpRequest();
+        articleRequest.onload = addArticles;
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key={api-key}`);
+        articleRequest.send();
+
+        function addArticles() {
+            let htmlContent = '';
+            const data = JSON.parse(this.responseText);
+
+            if (data.response && data.response.docs && data.response.docs.length > 1) {
+
+                htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article"><h2><a href="${article.web_url}">${article.headline.main}</a></h2><p>${article.snippet}</p></li>`).join('') + '</ul>';
+            } else {
+                htmlContent = '<div class="error-no-articles">No articles available</div>';
+            }
+            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+
+        }
+
+        //request images
+        const unsplashRequest = new XMLHttpRequest();
+        unsplashRequest.onload = addImage;
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID {api-key}');
+        unsplashRequest.send();
+
+        function addImage() {
+            let htmlContent = '';
+            const data = JSON.parse(this.responseText);
+
+            if (data && data.results && data.results[0]) {
+                const firstImage = data.results[0];
+                htmlContent = `<figure><img src="${firstImage.urls.regular}" alt="${searchedForText}"><figcaption>${searchedForText} by ${firstImage.user.name}</figcaption></figure>`;
+            } else {
+                htmlContent = '<div class="error-no-image">No images available</div>';
+            }
+            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        }
+
     });
 })();
